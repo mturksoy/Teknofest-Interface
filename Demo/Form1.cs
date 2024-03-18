142b854f2923a428d74f50ba1b32eee85c74a6c5
Muhammet Türksoy <mturksoy139@gmail.com>
2024-03-18 22:59:41 +0300

    Demo

---
+++ Demo/Form1.cs
@@ -0,0 +1,260 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class Form1 : Form
+    {
+        float x = 0, y = 0, z = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void silindir(float step, float topla, float radius, float dikey1, float dikey2)
+        {
+            float eski_step = 0.1f;
+            GL.Begin(BeginMode.Quads);//Y EKSEN CIZIM DAİRENİN
+            while (step <= 360)
+            {
+                if (step < 45)
+                    GL.Color3(Color.FromArgb(255, 0, 0));
+                else if (step < 90)
+                    GL.Color3(Color.FromArgb(255, 255, 255));
+                else if (step < 135)
+                    GL.Color3(Color.FromArgb(255, 0, 0));
+                else if (step < 180)
+                    GL.Color3(Color.FromArgb(255, 255, 255));
+                else if (step < 225)
+                    GL.Color3(Color.FromArgb(255, 0, 0));
+                else if (step < 270)
+                    GL.Color3(Color.FromArgb(255, 255, 255));
+                else if (step < 315)
+                    GL.Color3(Color.FromArgb(255, 0, 0));
+                else if (step < 360)
+                    GL.Color3(Color.FromArgb(255, 255, 255));
+
+
+                float ciz1_x = (float)(radius * Math.Cos(step * Math.PI / 180F));
+                float ciz1_y = (float)(radius * Math.Sin(step * Math.PI / 180F));
+                GL.Vertex3(ciz1_x, dikey1, ciz1_y);
+
+                float ciz2_x = (float)(radius * Math.Cos((step + 2) * Math.PI / 180F));
+                float ciz2_y = (float)(radius * Math.Sin((step + 2) * Math.PI / 180F));
+                GL.Vertex3(ciz2_x, dikey1, ciz2_y);
+
+                GL.Vertex3(ciz1_x, dikey2, ciz1_y);
+                GL.Vertex3(ciz2_x, dikey2, ciz2_y);
+                step += topla;
+            }
+            GL.End();
+            GL.Begin(BeginMode.Lines);
+            step = eski_step;
+            topla = step;
+            while (step <= 180)// UST KAPAK
+            {
+                if (step < 45)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 90)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+                else if (step < 135)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 180)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+                else if (step < 225)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 270)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+                else if (step < 315)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 360)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+
+
+                float ciz1_x = (float)(radius * Math.Cos(step * Math.PI / 180F));
+                float ciz1_y = (float)(radius * Math.Sin(step * Math.PI / 180F));
+                GL.Vertex3(ciz1_x, dikey1, ciz1_y);
+
+                float ciz2_x = (float)(radius * Math.Cos((step + 180) * Math.PI / 180F));
+                float ciz2_y = (float)(radius * Math.Sin((step + 180) * Math.PI / 180F));
+                GL.Vertex3(ciz2_x, dikey1, ciz2_y);
+
+                GL.Vertex3(ciz1_x, dikey1, ciz1_y);
+                GL.Vertex3(ciz2_x, dikey1, ciz2_y);
+                step += topla;
+            }
+            step = eski_step;
+            topla = step;
+            while (step <= 180)//ALT KAPAK
+            {
+                if (step < 45)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 90)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+                else if (step < 135)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 180)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+                else if (step < 225)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 270)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+                else if (step < 315)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 360)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+
+                float ciz1_x = (float)(radius * Math.Cos(step * Math.PI / 180F));
+                float ciz1_y = (float)(radius * Math.Sin(step * Math.PI / 180F));
+                GL.Vertex3(ciz1_x, dikey2, ciz1_y);
+
+                float ciz2_x = (float)(radius * Math.Cos((step + 180) * Math.PI / 180F));
+                float ciz2_y = (float)(radius * Math.Sin((step + 180) * Math.PI / 180F));
+                GL.Vertex3(ciz2_x, dikey2, ciz2_y);
+
+                GL.Vertex3(ciz1_x, dikey2, ciz1_y);
+                GL.Vertex3(ciz2_x, dikey2, ciz2_y);
+                step += topla;
+            }
+            GL.End();
+        }
+        private void koni(float step, float topla, float radius1, float radius2, float dikey1, float dikey2)
+        {
+            float eski_step = 0.1f;
+            GL.Begin(BeginMode.Lines);//Y EKSEN CIZIM DAİRENİN
+            while (step <= 360)
+            {
+                if (step < 45)
+                    GL.Color3(1.0, 1.0, 1.0);
+                else if (step < 90)
+                    GL.Color3(1.0, 0.0, 0.0);
+                else if (step < 135)
+                    GL.Color3(1.0, 1.0, 1.0);
+                else if (step < 180)
+                    GL.Color3(1.0, 0.0, 0.0);
+                else if (step < 225)
+                    GL.Color3(1.0, 1.0, 1.0);
+                else if (step < 270)
+                    GL.Color3(1.0, 0.0, 0.0);
+                else if (step < 315)
+                    GL.Color3(1.0, 1.0, 1.0);
+                else if (step < 360)
+                    GL.Color3(1.0, 0.0, 0.0);
+
+
+                float ciz1_x = (float)(radius1 * Math.Cos(step * Math.PI / 180F));
+                float ciz1_y = (float)(radius1 * Math.Sin(step * Math.PI / 180F));
+                GL.Vertex3(ciz1_x, dikey1, ciz1_y);
+
+                float ciz2_x = (float)(radius2 * Math.Cos(step * Math.PI / 180F));
+                float ciz2_y = (float)(radius2 * Math.Sin(step * Math.PI / 180F));
+                GL.Vertex3(ciz2_x, dikey2, ciz2_y);
+                step += topla;
+            }
+            GL.End();
+
+            GL.Begin(BeginMode.Lines);
+            step = eski_step;
+            topla = step;
+            while (step <= 180)// UST KAPAK
+            {
+                if (step < 45)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 90)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+                else if (step < 135)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 180)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+                else if (step < 225)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 270)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+                else if (step < 315)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 360)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+
+
+                float ciz1_x = (float)(radius2 * Math.Cos(step * Math.PI / 180F));
+                float ciz1_y = (float)(radius2 * Math.Sin(step * Math.PI / 180F));
+                GL.Vertex3(ciz1_x, dikey2, ciz1_y);
+
+                float ciz2_x = (float)(radius2 * Math.Cos((step + 180) * Math.PI / 180F));
+                float ciz2_y = (float)(radius2 * Math.Sin((step + 180) * Math.PI / 180F));
+                GL.Vertex3(ciz2_x, dikey2, ciz2_y);
+
+                GL.Vertex3(ciz1_x, dikey2, ciz1_y);
+                GL.Vertex3(ciz2_x, dikey2, ciz2_y);
+                step += topla;
+            }
+            step = eski_step;
+            topla = step;
+            GL.End();
+        }
+         
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            float step = 1.0f;
+            float topla = step;
+            float radius = 5.0f;
+            float dikey1 = radius, dikey2 = -radius;
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Clear(ClearBufferMask.DepthBufferBit);
+
+            Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(1.04f, 4 / 3, 1, 10000);
+            Matrix4 lookat = Matrix4.LookAt(25, 0, 0, 0, 0, 0, 0, 1, 0);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.LoadMatrix(ref perspective);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+            GL.LoadMatrix(ref lookat);
+            GL.Viewport(0, 0, glControl1.Width, glControl1.Height);
+            GL.Enable(EnableCap.DepthTest);
+            GL.DepthFunc(DepthFunction.Less);
+
+            GL.Rotate(x, 1.0, 0.0, 0.0);//Yönlendirme kısımları
+            GL.Rotate(z, 0.0, 1.0, 0.0);
+            GL.Rotate(y, 0.0, 0.0, 1.0);
+
+            silindir(step, topla, radius, 3, -5);
+            koni(0.01f, 0.01f, radius, 3.0f, 3, 5);
+            koni(0.01f, 0.01f, radius, 2.0f, -5.0f, -10.0f);
+            
+
+            GL.Begin(BeginMode.Lines);
+
+            GL.Color3(Color.FromArgb(250, 0, 0));
+            GL.Vertex3(-30.0, 0.0, 0.0);
+            GL.Vertex3(30.0, 0.0, 0.0);
+
+
+            GL.Color3(Color.FromArgb(0, 0, 0));
+            GL.Vertex3(0.0, 30.0, 0.0);
+            GL.Vertex3(0.0, -30.0, 0.0);
+
+            GL.Color3(Color.FromArgb(0, 0, 250));
+            GL.Vertex3(0.0, 0.0, 30.0);
+            GL.Vertex3(0.0, 0.0, -30.0);
+
+            GL.End();
+            //GraphicsContext.CurrentContext.VSync = true;
+            glControl1.SwapBuffers();
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            GL.ClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            GL.Enable(EnableCap.DepthTest);//sonradan yazdık
+        }
+    }
+}
